e7ad671726cd9e080cbdd01c91046c3d641cca26
corre <petercoreiaalves@gmail.com>
2022-06-15 15:29:13 -0300

    primeiro commit

--- estoque/models.py
+++ estoque/models.py
@@ -4,11 +4,35 @@ from django.db import models
 
 class Estoque(models.Model):
 
-    nome = models.CharField(max_length=100)
-    endereco = models.CharField(max_length=100)
-    Produtos = models.CharField(max_length=100)
+    GUITARRA = 'GU'
+    VIOLAO = 'VI'
+    SOPROS = 'SO'
+    ACESSORIOS = 'AC'
+    PIANO = 'PI'
+    BATERIA = 'BA'
+    BAIXO = 'BI'
+    ARPAS = 'AR'
+
+    INSTRUMENTOS = [
+        (GUITARRA, 'Guitarra'),
+        (VIOLAO, 'Violão'),
+        (SOPROS, 'Sopros'),
+        (ACESSORIOS, 'Acessórios'),
+        (PIANO, 'Piano'),
+        (BATERIA, 'Bateria'),
+        (BAIXO, 'Baixo'),
+        (ARPAS, 'Arpas'),
+    ]
+
+
+    nome = models.CharField('Nome da Encomenda',max_length=100)
+    endereco = models.CharField('Endereço',max_length=100)
+    instru = models.CharField('Escolha o Produto', max_length=8, choices=INSTRUMENTOS, default=GUITARRA, )
     quantidade = models.BigIntegerField("Quantidade de Produtos",max_length=20)
     fornecedor = models.CharField(max_length=100)
 
     def str(self):
         return self.nome
+
+    def is_upperclass(self):
+        return self.instru
--- usuario/models.py
+++ usuario/models.py
@@ -5,11 +5,13 @@ from django.db import models
 class Usuario(models.Model):
 
     nome = models.CharField(max_length=100)
-    endereco = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
     telefone = models.BigIntegerField(max_length=14)
-    cpf = models.IntegerField(max_length=11)
-    nascimento = models.DateField(max_length=8)
+    endereco = models.CharField('Endereço', max_length=100)
+    nascimento = models.DateField('Data de Nascimento',max_length=8)
+    senha = models.CharField('Senha',max_length=8)
+    senha1 = models.CharField('Confirme sua senha',max_length=8)
+
 
     def str (self):
         return self.nome
